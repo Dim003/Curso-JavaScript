2bd241db08e081e6d8a84f5d27cc2a9cc8051982
Nicholas Adorni <nicholasadorni1643@gmail.com>
2022-03-17 13:50:56 -0300

    minha pasta (nick)

---
+++ Nickols/aula 7 - const/exe-aula7.js
@@ -0,0 +1,15 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC é de 25.925925925925924
+*/
+const nome = "Gordon";
+const sobrenome = 'Freeman';
+const idade = 69;
+const peso = 124;
+const altura = 1.80;
+let imc = peso/(altura*altura);
+let anoNascimento = 2022 - idade;
+
+console.log("IMC:",imc,"\nAno de Nascimento:",anoNascimento);
+console.log(`Nome: ${Gordon} \n Idade: ${idade}`) // so funciona se usar crase ( ` )
+
