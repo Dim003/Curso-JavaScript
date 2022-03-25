8c928d20b97c3de1104acf44f81d767dfee479c5
Dim003 <fernandozocarato@hotmail.com>
2022-03-25 20:13:14 -0300

    i

--- aula 12/index.js
+++ aula 12/index.js
@@ -8,5 +8,5 @@ texto.innerHTML += `<p>Raiz quadrada:${numero **0.5}</p>`
 texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p>`
 texto.innerHTML += `<p>é NaN: ${Number.isNaN(numero)}</p>`
 texto.innerHTML += `<p>Arrendondando para baixo: ${Math.floor(numero)}</p>`
-texto.innerHTML += `<p>Arrendondando para baixo: ${Math.ceil(numero)}</p>`
+texto.innerHTML += `<p>Arrendondando para cima: ${Math.ceil(numero)}</p>`
 texto.innerHTML += `<p>Com duas casas decimais : ${numero.toFixed(2)}</p>`
